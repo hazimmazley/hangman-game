0528c083ff2fefda5cec0372b32195a0b61f66c4
Hazim Mazley <hazimmazley@gmail.com>
2022-07-15 21:29:05 +0800

    letter press event handler

--- script.js
+++ script.js
@@ -32,4 +32,40 @@ function displayWord() {
     }
 }
 
+function updateWrongLettersEl() {
+
+}
+
+function showNotification() {
+    notification.classList.add('show');
+
+    setTimeout(() => {
+      notification.classList.remove('show');
+    }, 2000);
+}
+
+// Keydown letter press
+window.addEventListener('keydown', e => {
+    if (e.keyCode >= 65 &&  e.keyCode <= 90) {
+        const letter = e.key;
+
+        if (selectedWord.includes(letter)) {
+            if(!correctLetters.includes(letter)) {
+                correctLetters.push(letter)
+                displayWord()
+            } else { 
+                showNotification()
+            }
+        } else {
+            if (!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter)
+
+                updateWrongLettersEl()
+            } else {
+                showNotification()
+            }
+        }
+    }
+});
+
 displayWord()
