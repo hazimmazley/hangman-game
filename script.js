dfdc0ea028bccb7449603c55613a91c8925dbf6e
Hazim Mazley <hazimmazley@gmail.com>
2022-07-15 21:47:14 +0800

    wrong letters and play again

--- script.js
+++ script.js
@@ -33,7 +33,28 @@ function displayWord() {
 }
 
 function updateWrongLettersEl() {
+    // Display wrong letters
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
 
+    // Display parts
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length;
+
+        if (index < errors) {
+        part.style.display = 'block';
+        } else {
+        part.style.display = 'none';
+        }
+    });
+
+    // Check if lost
+    if (wrongLetters.length === figureParts.length) {
+        finalMessage.innerText = 'Unfortunately you lost. 😕';
+        popup.style.display = 'flex';
+    }
 }
 
 function showNotification() {
@@ -67,5 +88,21 @@ window.addEventListener('keydown', e => {
         }
     }
 });
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+    //  Empty arrays
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+  
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+  
+    displayWord();
+  
+    updateWrongLettersEl();
+  
+    popup.style.display = 'none';
+  });
+  
 
 displayWord()
